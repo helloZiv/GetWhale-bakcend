03ecf29faad0ea5eca752cfe218645e9d1674282
Ziv Chan <zivchan.tw@gmail.com>
2022-06-23 14:37:13 +0800

    first commit

---
+++ src/main/java/com/example/getwhaledata/GetwhaledataApplication.java
@@ -0,0 +1,13 @@
+package com.example.getwhaledata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwhaledataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetwhaledataApplication.class, args);
+    }
+
+}
